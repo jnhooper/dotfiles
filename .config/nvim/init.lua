39e472bfafad5e1d82679e3cac997b8767c5a20f
John Hooper <nisse038@gmail.com>
2026-09-04 11:36:44 -0700

    harper

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -737,6 +737,15 @@ require('lazy').setup({
 
       vim.lsp.enable 'denols'
 
+      vim.lsp.config('harper_ls', {
+        settings = {
+          ['harper-ls'] = {
+            userDictPath = '~/dict.txt',
+          },
+        },
+      })
+      vim.lsp.enable 'harper_ls'
+
       -- local mason_registry = require 'mason-registry'
       -- local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
       local vue_language_server_path = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server'
